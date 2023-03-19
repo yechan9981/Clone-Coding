a59a5c6d66653b36283d28dc20486ef1a404f2bc
yechan Cheon <yechan9981@naver.com>
2023-03-20 00:25:46 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/tonnflix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tonnflix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class Player {
+  String name;
+  Player(this.name);
+}
+
+void main() {
+  var nico  = Player("nico");
+  
+  runApp(App());
+}
+
+class App extends StatelessWidget{
+   
+   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 
+          40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.
+            start,
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment. 
+              end,
+              children: [
+                Column(
+                  crossAxisAlignment:
+                  CrossAxisAlignment.end,
+                  children: [
+                    Text('Hey, Selena',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Text('Welcome back',
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balance',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white.withOpacity(0.8)
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+             Text(
+              '\$5 194 482',
+              style: TextStyle(
+                fontSize: 44,
+                fontWeight: FontWeight.w600,
+                color: Colors.white
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.
+                    circular(45)
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 50,
+                    ),
+                    child: Text('Transfer',
+                      style: TextStyle(
+                        fontSize: 22
+                      ),
+                    ),
+                  ),
+                )
+
+              ],
+            )
+
+          ],
+        ),),
+      ),
+    );
+  }
+}
